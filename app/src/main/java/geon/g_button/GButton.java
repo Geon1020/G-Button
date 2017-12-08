14a33986ddd34ac7b33183aa96fb4b68cb801706
Geon <Geon.416@gmail.com>
2017-12-09 01:50:12 +0900

    first commit

---
+++ app/src/main/java/geon/g_button/GButton.java
@@ -0,0 +1,19 @@
+package geon.g_button;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+public class GButton extends android.support.v7.widget.AppCompatButton {
+
+    public GButton(Context context) {
+        super(context);
+    }
+
+    public GButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public GButton(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
