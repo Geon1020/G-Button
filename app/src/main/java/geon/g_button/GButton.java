14bcb69fc20d7e135888a0220cc0f918efbab631
Geon <Geon.416@gmail.com>
2017-12-09 10:10:18 +0900

    test커밋

--- app/src/main/java/geon/g_button/GButton.java
+++ app/src/main/java/geon/g_button/GButton.java
@@ -1,19 +1,39 @@
 package geon.g_button;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.StateListDrawable;
 import android.util.AttributeSet;
 
 public class GButton extends android.support.v7.widget.AppCompatButton {
 
     public GButton(Context context) {
         super(context);
+
+        init();
     }
 
     public GButton(Context context, AttributeSet attrs) {
         super(context, attrs);
+
+        init();
     }
 
     public GButton(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+
+        init();
+    }
+
+    private void init() {
+        setBackground(getResources().getDrawable(R.mipmap.ic_launcher));
+    }
+
+    public StateListDrawable drawPress(Drawable normal, Drawable pressed) {
+        StateListDrawable drawable = new StateListDrawable();
+        drawable.addState(new int[] {-android.R.attr.state_pressed}, normal); // Normal
+        drawable.addState(new int[] {android.R.attr.state_pressed}, pressed); // Pressed
+
+        return drawable;
     }
 }
